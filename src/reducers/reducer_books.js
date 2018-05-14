bdd8be8012eaae53f3593d6a7f20d5ccdd5df6d2
Untitled <john.d.reddock@gmail.com>
2018-05-14 14:21:10 -0400

    book list container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        {title: 'Javascript 101'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
+
